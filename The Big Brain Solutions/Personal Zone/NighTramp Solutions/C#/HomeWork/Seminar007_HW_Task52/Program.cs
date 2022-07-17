f94177d8bc07f794ccb1a4773802d0fea11f71d2
NighTramp <nightramp@yandex.ru>
2022-07-17 20:12:09 +0400

    Add complited Task52

---
+++ The Big Brain Solutions/Personal Zone/NighTramp Solutions/C#/HomeWork/Seminar007_HW_Task52/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+namespace Task52
+{
+    class Program
+    {
+        static int[,] array = {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}};
+        static float[] average_array = new float[array.GetUpperBound(1) + 1];
+        static void FindAverageOfColumnsOf2DArray()
+        {
+            for (int j = 0; j <= array.GetUpperBound(1); j++)
+            {
+                int i;
+                for (i = 0; i <= array.GetUpperBound(0); i++)
+                {
+                    average_array[j] += array[i,j];
+                }
+                average_array[j] = average_array[j] / (i);
+            }
+        }
+        static void ViewAverage()
+        {
+            Console.Write("Среднее арифметическое каждого столбца: ");
+            for (int i = 0; i < average_array.Length; i++)
+            {
+                Console.Write("{0}; ", Math.Round(average_array[i], 1));
+            }
+        }
+        static void Main(string[] asgs)
+        {
+            FindAverageOfColumnsOf2DArray();
+            ViewAverage();
+        }
+    }
+}
