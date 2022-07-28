c64495e96391d30ba7c88fb70bf8c46463e3ad33
NighTramp <nightramp@yandex.ru>
2022-07-28 12:51:46 +0400

    Add complited Task68

---
+++ The Big Brain Solutions/Personal Zone/NighTramp Solutions/C#/HomeWork/Seminar009_HW_Task68/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29
+*/
+namespace Task68
+{
+    class Program
+    {
+        static int AckermanF(int m, int n)
+        {
+            if (m == 0) return n + 1;
+            else if ((m != 0) && (n == 0)) return AckermanF(m - 1, 1);
+            else return AckermanF(m - 1, AckermanF(m, n - 1));
+        }
+        static void Main(string[] asgs)
+        {
+            Console.Write("Введите первое число: ");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int result = AckermanF(M, N);
+            Console.WriteLine(result);
+        }
+    }
+}
